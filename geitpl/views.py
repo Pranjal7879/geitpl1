f5c73100c4fb16244ae65a2342e8fb074d1e1371
pranjal shukla <138642719+Pranjal7879@users.noreply.github.com>
2025-03-28 17:48:23 +0530

    firstcommit

---
+++ geitpl/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homePage(request):
+    data={
+        'title':'GEITPL',
+        'bdata':'Welcome to office',
+        'clist' : ["js", "django", "python", "github"],
+        'students_detail':[
+            {'name':'pranjal', 'phone': 1345678988},
+            {'name':'shukla', 'phone': 98745632159}
+
+        ]
+    }
+    return render(request,"index.html",data)
+
+def aboutUs(request):
+    return HttpResponse("this is my home page ")
+
+def courses(request):
+    return HttpResponse("welcome to my course")
+
+
+def Coursedetail(request,courseid):
+    return HttpResponse(courseid)
